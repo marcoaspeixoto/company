4687ecaff8284f0ea78e0b6517d16cc6cd8846fd
Marco Peixoto <marcoaspeixto@gmail.com>
2022-10-01 08:21:21 -0300

    feat(project): scaffold and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_01_111920) do
+ActiveRecord::Schema.define(version: 2022_10_01_112053) do
 
   create_table "employees", force: :cascade do |t|
     t.string "fname"
     t.string "iname"
     t.date "dob"
     t.string "empid"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
